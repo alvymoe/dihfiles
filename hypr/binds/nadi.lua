a01214e58bbf9ae87ea6d0517a779d67a923cb2d
nnadire <soulinake@gmail.com>
2026-08-19 01:55:20 +0100

    Update nadi.lua

--- hypr/binds/nadi.lua
+++ hypr/binds/nadi.lua
@@ -104,7 +104,6 @@ hl.config({
         kb_options = "",
         kb_rules = "",
         follow_mouse = 1,
-        sensitivity = -1, -- -1.0 - 1.0, 0 means no modification.
         touchpad = {
             natural_scroll = true,
             scroll_factor = 0.2,
